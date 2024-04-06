dfc20dddc18a803a72a7ca027b3085d4ecb24555
Ryo-Seah <seah_ryo@yahoo.com.sg>
2024-04-06 16:29:05 +0800

    add minRemoveToMakeValid Python

---
+++ leetCode/minRemoveToMakeValid/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+        s_list = list(s)
+        for i, char in enumerate(s_list):
+            if char == '(':
+                stack.append(i)
+            elif char == ')':
+                if stack:
+                    stack.pop()
+                else: 
+                    s_list[i] = ''
+        
+        for idx in stack:
+            s_list[idx] = ''
+        return ''.join(s_list)
+
+
+
+
+        
